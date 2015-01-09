51ca6ada1859d3dee46e6ceb84600f61ea9e5393
Dmitry Ledentsov <dmlled@yahoo.com>
2015-01-09 09:54:05 +0100

    windows build

--- make_vs2008.bat
+++
@@ -1,3 +0,0 @@
-premake4 clean
-premake4 vs2008
-pause
--- premake4.lua
+++ premake4.lua
@@ -1,92 +1,41 @@
-local OS=os.get()
+include 'premake'
 
-local cmd = {
- dir =     { linux = "ls", windows = "dir", macosx = "ls" },
- libdirs = { linux = { "" }, windows = { "./Lua/lib" }, macosx = { "" } },
- includedirs = { linux = { "/usr/include/lua5.1"}, windows = { "./Lua/include", os.getenv("BOOST") }, macosx = { "" } },
- links = { linux = { "lua5.1-c++" }, windows = { "lua5.1" }, macosx = { "lua","c++" } },
- location = { linux = "Build", windows = "Build", macosx = "BuildClang" },
- buildoptions = { linux = "-v -std=gnu++0x -fPIC", windows = "-v -std=c++11 -fPIC", macosx = "-v -stdlib=libc++ -std=c++11 -fPIC" }
+make_solution 'pugilua'
+
+platforms { "native","x32", "x64" }
+
+includedirs { 
+    './LuaBridge-1.0.2',
+    './pugixml/src'
 }
 
-local cfg={}
-
-for i,v in pairs(cmd) do
- cfg[i]=cmd[i][OS]
-end
-
--- Apply to current "filter" (solution/project)
-function DefaultConfig()
-	location "Build"
-	configuration "Debug"
-		defines { "DEBUG", "_DEBUG" }
-		objdir "Build/obj"
-		targetdir "./Lua/lib"
-		targetprefix ""
-		flags { "Symbols" }
-	configuration "Release"
-		defines { "RELEASE" }
-		objdir "Build/obj"
-		targetdir "./Lua/lib"
-		targetprefix ""
-		flags { "Optimize" }
-	configuration "*" -- to reset configuration filter
-end
-
-function CompilerSpecificConfiguration()
-	configuration {"xcode*" }
-		postbuildcommands {"$TARGET_BUILD_DIR/$TARGET_NAME"}
-
-	configuration {"gmake"}
-		postbuildcommands  { "$(TARGET)" }
-		buildoptions { "-v -std=gnu++0x -fPIC" }
-
-	configuration {"codeblocks" }
-		postbuildcommands { "$(TARGET_OUTPUT_FILE)"}
-
-	configuration { "vs*"}
-		postbuildcommands { "\"$(TargetPath)\"" }
-end
-
-----------------------------------------------------------------------------------------------------------------
-
-newaction {
-   trigger     = "run",
-   description = "run lua",
-   execute     = function ()
-      os.execute("lua -l pugilua")
-   end
+local OS = os.get()
+local settings = {
+    includedirs = {
+        linux = {'/usr/include/lua5.1'},
+        windows = { [[C:\\luarocks\\2.1\\include]] },
+        macosx = { '/usr/local/include'}
+    },
+    libdirs = {
+        linux = {},
+        windows = { [[C:\\luarocks\\2.1]] },
+        macosx = { '/usr/local/lib'}	
+    },
+    links = {
+        linux = { 'lua5.1' },
+        windows = { 'lua5.1' },
+        macosx = { 'lua' }
+    }
 }
 
-----------------------------------------------------------------------------------------------------------------
+includedirs { settings.includedirs[OS] }
+libdirs { settings.libdirs[OS] }
 
--- A solution contains projects, and defines the available configurations
-local sln=solution "pugilua"
-	location "Build"
-		sln.absbasedir=path.getabsolute(sln.basedir)
-		configurations { "Debug", "Release" }
-		platforms { "native","x32", "x64" }
-		libdirs ( cfg.libdirs )
-		includedirs ( cfg.includedirs )
-		includedirs {
-			[[./LuaBridge/Source/LuaBridge]],
-			[[./pugixml/src]]
-		}
-		vpaths {
-			["Headers"] = {"**.h","**.hpp"},
-			["Sources"] = {"**.c", "**.cpp"},
-		}
-
-----------------------------------------------------------------------------------------------------------------
-   local dll=project "pugilua"
-   location "Build"
-		kind "SharedLib"
-		DefaultConfig()
-		language "C++"
-		files {
+make_shared_lib('pugilua', {
 			"./pugilua/*.h",
 			"./pugilua/*.cpp",
 			"./pugixml/src/*.hpp",
 			"./pugixml/src/*.cpp"
-		}
-		links(cfg.links)
+})
+
+links( settings.links[OS] )
